e1f8c74a701d067b710994dd87e64d2e2cebac60
sandeepreddygantla <reddysandeep0904@gmail.com>
2024-04-17 11:10:47 -0500

    updated packages

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa_Approval",
+    version="0.0.0",
+    author="Sandeep Reddy",
+    author_email="gantlasandeep98@gmail.com",
+    packages=find_packages()
+)
